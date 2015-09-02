0e9dc96a4c82838d8615716652b10d73aaa51682
Mike Jones <mikethespike77@gmail.com>
2015-09-02 17:46:34 +0100

    initial commit

---
+++ app.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry'
+get '/' do
+	@title = 'Home'
+	erb :home
+end
+
+post '/navigate' do
+	case params[:destination].downcase
+	when 'simple' then redirect to '/simple'
+	when 'advanced' then redirect to '/advanced'
+	when 'bmi' then redirect to '/bmi'
+	when 'mortgage' then redirect to '/mortgage'
+	when 'trip' then redirect to '/trip'
+	else 
+		@error = 'Invalid calculator selected'
+		erb :home
+	end
+end
+
+get '/simple' do
+	@title = 'Simple'
+	erb :simple
+end
+
+get '/advanced' do
+	@title = 'Advanced'
+	erb :advanced
+end
+
+get '/bmi' do
+	@title = 'Bmi'
+	erb :bmi
+end
+
+get '/trip' do
+	@title = 'Trip'
+	erb :trip
+end
+
+
